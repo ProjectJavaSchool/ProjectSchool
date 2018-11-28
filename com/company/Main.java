57ad7b4b0af156da8aadb73df07f23c9e078027f
Hoang Nguyen Phuoc Bao <ngpbhoang1406@gmail.com>
2018-11-29 06:08:01 +0700

    Revert "Sua di Thao oi :)"
    This reverts commit 4ae6b031f547f3cf84f68015adb0aaaedb191a25.

--- com/company/Main.java
+++ com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
-    //cai nay anh cung chua coi nha
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Student student = new Student();
--- com/company/PersonInSchool.java
+++ com/company/PersonInSchool.java
@@ -1,40 +1,12 @@
 package com.company;
 
-import org.omg.CORBA.portable.IDLEntity;
-
 import java.util.Date;
 
-//Class nay abstract nha em!!
-public abstract class PersonInSchool {
-//public class PersonInSchool {
-
-    //cac bien cua em phai dung private nhe, neu em de nhu vay thi khong nen.
-    private String id;
-    //vi bien nay cua em private nen em can co getter va setter
-    //setter la de em set cai gia tri vao bien do
-    public void setId(String id) {
-        this.id = id;
-    }
-    //getter la de em lay gia tri do ra
-    public String getId() {
-        return this.id;
-    }
-
-    //tuong tu cho may cai duoi nha, ly do tai sao thi em co the google, neu van k hieu thi hoi anh
+public class PersonInSchool {
+    String id;
     String name;
     Date dob;
     String email;
     String phone;
     String addr;
-
-    //em can them mot cai constructor nua
-
-    public PersonInSchool(String id, String name, Date dob, String email, String phone, String addr) {
-        this.id = id;
-        this.name = name;
-        this.dob = dob;
-        this.email = email;
-        this.phone = phone;
-        this.addr = addr;
-    }
 }
