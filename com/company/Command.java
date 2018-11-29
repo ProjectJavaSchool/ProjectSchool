c4ba64ec61bf087f4111aa133dd9bd2f2267228b
Hoang Nguyen Phuoc Bao <ngpbhoang1406@gmail.com>
2018-11-29 21:02:38 +0700

    Command constants class commit

---
+++ com/company/Command.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Command {
+    public static final String SEPERATOR = "===================================";
+    public static final String MANAGE_STUDENTS = "Manage Students";
+    public static final String MANAGE_LECTURERS = "Manage Lecturers";
+    public static final String EXIT = "Exit";
+    public static final String ADD_NEW_STUDENT = "Add new student";
+    public static final String VIEW_ALL_STUDENTS = "View all students";
+    public static final String SEARCH_STUDENTS = "Search students";
+    public static final String DELETE_STUDENTS = "Delete students";
+    public static final String UPDATE_STUDENT = "Update student";
+    public static final String BACK_TO_MAIN_MENU = "Back to main menu";
+    public static final String ADD_NEW_LECTURER = "Add new lecturer";
+    public static final String VIEW_ALL_LECTURERS = "View all lecturers";
+    public static final String SEARCH_LECTURERS = "Search lecturers";
+    public static final String DELETE_LECTURERS = "Delete lecturers";
+    public static final String UPDATE_LECTURER = "Update lecturer";
+}
