c7903569e97a458c8e5ab38214f7d6c4e211a742
Hoang Nguyen Phuoc Bao <ngpbhoang1406@gmail.com>
2018-11-29 21:38:38 +0700

    First application window

--- com/company/Command.java
+++ com/company/Command.java
@@ -2,18 +2,31 @@ package com.company;
 
 public class Command {
     public static final String SEPERATOR = "===================================";
-    public static final String MANAGE_STUDENTS = "Manage Students";
-    public static final String MANAGE_LECTURERS = "Manage Lecturers";
-    public static final String EXIT = "Exit";
-    public static final String ADD_NEW_STUDENT = "Add new student";
-    public static final String VIEW_ALL_STUDENTS = "View all students";
-    public static final String SEARCH_STUDENTS = "Search students";
-    public static final String DELETE_STUDENTS = "Delete students";
-    public static final String UPDATE_STUDENT = "Update student";
-    public static final String BACK_TO_MAIN_MENU = "Back to main menu";
-    public static final String ADD_NEW_LECTURER = "Add new lecturer";
-    public static final String VIEW_ALL_LECTURERS = "View all lecturers";
-    public static final String SEARCH_LECTURERS = "Search lecturers";
-    public static final String DELETE_LECTURERS = "Delete lecturers";
-    public static final String UPDATE_LECTURER = "Update lecturer";
+    public static final String MANAGE_STUDENTS = "1. Manage Students";
+    public static final String MANAGE_LECTURERS = "2. Manage Lecturers";
+    public static final String EXIT = "3. Exit";
+    public static final String ADD_NEW_STUDENT = "1. Add new student";
+    public static final String VIEW_ALL_STUDENTS = "2. View all students";
+    public static final String SEARCH_STUDENTS = "3. Search students";
+    public static final String DELETE_STUDENTS = "4. Delete students";
+    public static final String UPDATE_STUDENT = "5. Update student";
+    public static final String BACK_TO_MAIN_MENU = "6. Back to main menu";
+    public static final String ADD_NEW_LECTURER = "1. Add new lecturer";
+    public static final String VIEW_ALL_LECTURERS = "2. View all lecturers";
+    public static final String SEARCH_LECTURERS = "3. Search lecturers";
+    public static final String DELETE_LECTURERS = "4. Delete lecturers";
+    public static final String UPDATE_LECTURER = "5. Update lecturer";
+
+    public static final String BLANK_LINE = "\n";
+
+    public static final String NOT_VALID_NUMBER_INPUT = "You input is not valid. Please input again";
+    public static final String ENTER_YOUR_OPTION = "Enter your option: ";
+
+    public static void writeln(String content) {
+        System.out.println(content);
+    }
+
+    public static void write(String content) {
+        System.out.print(content);
+    }
 }
--- com/company/Main.java
+++ com/company/Main.java
@@ -1,15 +1,69 @@
 package com.company;
 
 import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import static com.company.Command.*;
 
 public class Main {
+    private static Scanner scan = initScanner();
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        openFirstApplicationWindow();
+        readMainOption();
+
+
         Student student = new Student();
         student.set("GT53638", "Jason", new Date(), "jason@email.com", "03423454567", "fgfccv", "lala");
         Lecturer lecturer;
         School school;
+    }
+
+    private static void readMainOption() {
+        int optionNumber = -1;
+        do {
+            optionNumber = getOptionNumber();
+            if(optionNumber == 3) break;
+            else if(optionNumber > 0 && optionNumber < 3) {
+                if(optionNumber == 1) gotoMangeStudentWindow();
+                else gotoManageLecturerWindow();
+                openFirstApplicationWindow();
+            }
+            else informInvalidNumberInput();
+        } while(optionNumber != 3);
+    }
+
+    private static void gotoMangeStudentWindow() {
+    }
+
+    private static void gotoManageLecturerWindow() {
+    }
+
+    private static int getOptionNumber() {
+        try {
+            return scan.nextInt();
+        } catch (InputMismatchException e) {
+            scan.nextLine();
+            return -1;
+        }
+
+    }
+
+    private static void informInvalidNumberInput() {
+        writeln(NOT_VALID_NUMBER_INPUT);
+        write(ENTER_YOUR_OPTION);
+    }
+
+    private static void openFirstApplicationWindow() {
+        writeln(SEPERATOR);
+        writeln(MANAGE_STUDENTS);
+        writeln(MANAGE_LECTURERS);
+        writeln(EXIT);
+        writeln(SEPERATOR);
+        write(ENTER_YOUR_OPTION);
+    }
+
+    private static Scanner initScanner() {
+        return new Scanner(System.in);
     }
 }
